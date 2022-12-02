8b683c9593c17b8d092bdc82c8ea58541781852f
girolamo <58935223+girolamodaschio@users.noreply.github.com>
2022-12-02 08:52:00 +0000

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI, status
+from fastapi.responses import JSONResponse
+
+import datetime
+
+import uvicorn as uvicorn
+
+
+app = FastAPI()
+
+
+@app.get("/healthcheck")
+def healthcheck():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"Status": "Running"})
+
+
+@app.get("/")
+def main():
+    xmas = datetime.datetime(datetime.datetime.now().year, 12, 25)
+    today = datetime.datetime.now()
+    days_to_xmas = (xmas - today).days
+
+    return {'Days' : days_to_xmas}
+
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=9090, log_level="warning")
