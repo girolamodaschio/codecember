d8289aee515e28f7535abf871ddf99bd7ce20c4a
girolamo <58935223+girolamodaschio@users.noreply.github.com>
2023-11-29 11:15:07 +0100

    codecember 2023 - first commit

--- main.py
+++ main.py
@@ -1,26 +1,46 @@
-import datetime
+import sys
+from pyicloud import PyiCloudService
 
-from fastapi import FastAPI, status
-from fastapi.responses import JSONResponse
-import uvicorn
+from src.config import ACCOUNT, PASSWORD
 
+api = PyiCloudService(ACCOUNT, PASSWORD)
+if api.requires_2fa:
+    print("Two-factor authentication required.")
+    code = input("Enter the code you received of one of your approved devices: ")
+    result = api.validate_2fa_code(code)
+    print("Code validation result: %s" % result)
 
-app = FastAPI()
+    if not result:
+        print("Failed to verify security code")
+        sys.exit(1)
 
+    if not api.is_trusted_session:
+        print("Session is not trusted. Requesting trust...")
+        result = api.trust_session()
+        print("Session trust result %s" % result)
 
-@app.get("/healthcheck")
-def healthcheck():
-    return JSONResponse(status_code=status.HTTP_200_OK, content={"Status": "Running"})
+        if not result:
+            print("Failed to request trust. You will likely be prompted for the code again in the coming weeks")
+elif api.requires_2sa:
+    import click
+    print("Two-step authentication required. Your trusted devices are:")
 
+    devices = api.trusted_devices
+    for i, device in enumerate(devices):
+        print(
+            "  %s: %s" % (i, device.get('deviceName',
+            "SMS to %s" % device.get('phoneNumber')))
+        )
 
-@app.get("/")
-def main():
-    xmas = datetime.datetime(datetime.datetime.now().year, 12, 25)
-    today = datetime.datetime.now()
-    days_to_xmas = (xmas - today).days
+    device = click.prompt('Which device would you like to use?', default=0)
+    device = devices[device]
+    if not api.send_verification_code(device):
+        print("Failed to send verification code")
+        sys.exit(1)
 
-    return {'Days' : days_to_xmas}
+    code = click.prompt('Please enter validation code')
+    if not api.validate_verification_code(device, code):
+        print("Failed to verify verification code")
+        sys.exit(1)
 
-
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=9090, log_level="warning")
+print('Login was successful')
---
+++ src/config.py
@@ -0,0 +1,4 @@
+from decouple import config
+
+ACCOUNT = config("APPLE_ID")
+PASSWORD = config("PASSWORD")
